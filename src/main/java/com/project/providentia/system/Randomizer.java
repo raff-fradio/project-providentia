178a7748378355b73387add2b11d9b35da158b3f
Juan <commandjuan@yahoo.com>
2020-10-21 21:19:24 +0700

    Update Randomizer.java

--- src/main/java/com/project/providentia/system/Randomizer.java
+++ src/main/java/com/project/providentia/system/Randomizer.java
@@ -1,8 +1,8 @@
 package com.project.providentia.system;
 
-import java.util.*;
+import java.util.Random;
 
-public class Randomizer {
+public class Randomizer implements Runnable{
 	public double getRandomTemp(double min, double max) {
 		return (new Random().nextDouble()*(max-min)+min);
 	}
@@ -11,5 +11,21 @@ public class Randomizer {
 	}
 	public Boolean getRandomFrontDoor() {
 		return (new Random().nextBoolean());
+	}
+	public void insertVar() {
+		
+		
+	}
+	@Override
+	public void run() {
+		Activity activity = new Activity();	
+		try {
+			do {
+				
+				Thread.sleep(10000);
+			} while(true);
+		} catch(InterruptedException e) {
+			System.out.println("Error !");
+		}
 	}
 }
