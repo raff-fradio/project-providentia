ca449950d51036a48564b557ffa8138f32e5ea48
Raffael Fradio <raffael.fradio@student.sgu.ac.id>
2020-10-21 14:12:04 +0700

    Test HelloFX

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.project.providentia {
     requires javafx.controls;
     requires javafx.fxml;
+	requires javafx.graphics;
 
     opens com.project.providentia to javafx.fxml;
     exports com.project.providentia;
