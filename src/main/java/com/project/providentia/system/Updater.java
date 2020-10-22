36655b4ab95f3d44c08828a7dc02f143ce622dc2
Raffael Fradio <51884830+raff-fradio@users.noreply.github.com>
2020-10-22 19:54:45 +0700

    Test observer

--- src/main/java/com/project/providentia/system/Updater.java
+++ src/main/java/com/project/providentia/system/Updater.java
@@ -2,15 +2,19 @@ package com.project.providentia.system;
 
 public class Updater implements Runnable {
 
+	Activity activity = new Activity();
 	Randomizer randomizer = new Randomizer();
 	
 	@Override
 	public void run() {
 		while (true) {
 			try {
-				System.out.println("Front Door Sensor: " + randomizer.getRandomFrontDoor());
-				System.out.println("Temperature: " + randomizer.getRandomTemperature());
-				System.out.println("Wind Speed: " + randomizer.getRandomWindSpeed());
+//				System.out.println("Front Door Sensor: " + randomizer.getRandomFrontDoor());
+//				System.out.println("Temperature: " + randomizer.getRandomTemperature());
+//				System.out.println("Wind Speed: " + randomizer.getRandomWindSpeed());
+				
+				activity.setVariables(randomizer.getRandomTemperature(), randomizer.getRandomWindSpeed(), randomizer.getRandomFrontDoor());
+				
 				System.out.println();
 				Thread.sleep(5000);
 			} catch (InterruptedException e) {
