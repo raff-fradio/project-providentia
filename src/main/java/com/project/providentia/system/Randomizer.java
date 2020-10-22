b0ee1333f47d762ebf8310ded7fa0e77bdbc1395
Raffael Fradio <51884830+raff-fradio@users.noreply.github.com>
2020-10-22 18:57:57 +0700

    Testing Observer

--- src/main/java/com/project/providentia/system/Randomizer.java
+++ src/main/java/com/project/providentia/system/Randomizer.java
@@ -5,24 +5,35 @@ import java.util.Random;
 public class Randomizer {
 	
 	private final double MIN_TEMP = -10;
-	private final double MAX_TEMP = 30;
+	private final double MAX_TEMP = 40;
 	private final double MIN_WIND_SPEED = 0;
 	private final double MAX_WIND_SPEED = 370;
 	private final double GUEST_CHANCE = 0.2;
+	
+	private final int DECIMAL_PLACE = 2;
 		
-	Random random = new Random();
+	private Random random;
+	
+	public Randomizer() {
+		this.random = new Random();
+	}
 	
 	public double getRandomTemperature() {
-		return random.nextDouble() * (MAX_TEMP - MIN_TEMP) * MIN_TEMP;
+		return round(random.nextDouble() * (MAX_TEMP - MIN_TEMP) + MIN_TEMP);
 	}
 	
 	public double getRandomWindSpeed() {
-		return random.nextDouble() * (MAX_WIND_SPEED - MIN_WIND_SPEED) + MIN_WIND_SPEED;
+		return round(random.nextDouble() * (MAX_WIND_SPEED - MIN_WIND_SPEED) + MIN_WIND_SPEED);
 	}
 	
 	public boolean getRandomFrontDoor() {
 		double randomDouble = random.nextDouble();
 		if (randomDouble < GUEST_CHANCE) return true;
 		else return false;
+	}
+	
+	private double round(double value) {
+		double scale = Math.pow(10, DECIMAL_PLACE);
+		return Math.round(value * scale) / scale;
 	}
 }
