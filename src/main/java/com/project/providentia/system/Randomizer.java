5d82c80094179b4a2499cb3d018f2bf83e007f96
Raffael Fradio <51884830+raff-fradio@users.noreply.github.com>
2020-10-22 18:14:30 +0700

    Renaming variables

--- src/main/java/com/project/providentia/system/Randomizer.java
+++ src/main/java/com/project/providentia/system/Randomizer.java
@@ -10,19 +10,19 @@ public class Randomizer {
 	private final double MAX_WIND_SPEED = 370;
 	private final double GUEST_CHANCE = 0.2;
 		
-	Random randomizer = new Random();
+	Random random = new Random();
 	
 	public double getRandomTemperature() {
-		return randomizer.nextDouble() * (MAX_TEMP - MIN_TEMP) * MIN_TEMP;
+		return random.nextDouble() * (MAX_TEMP - MIN_TEMP) * MIN_TEMP;
 	}
 	
 	public double getRandomWindSpeed() {
-		return randomizer.nextDouble() * (MAX_WIND_SPEED - MIN_WIND_SPEED) + MIN_WIND_SPEED;
+		return random.nextDouble() * (MAX_WIND_SPEED - MIN_WIND_SPEED) + MIN_WIND_SPEED;
 	}
 	
 	public boolean getRandomFrontDoor() {
-		double random = randomizer.nextDouble();
-		if (random < GUEST_CHANCE) return true;
+		double randomDouble = random.nextDouble();
+		if (randomDouble < GUEST_CHANCE) return true;
 		else return false;
 	}
 }
