3924a66b70f86892b9498a11640b838bf6b24bde
Raffael Fradio <51884830+raff-fradio@users.noreply.github.com>
2020-10-22 23:34:57 +0700

    Update Control.java

--- src/main/java/com/project/providentia/system/Control.java
+++ src/main/java/com/project/providentia/system/Control.java
@@ -1,29 +1,6 @@
 package com.project.providentia.system;
 public class Control {
-	double temp;
-	Boolean roomLight;
-	Boolean blind;
-//	ArrayList<Light> light = new ArrayList<Light>();
-	public double getTemp() {
-		return temp;
-	}
-	public void setTemp(double temp) {
-		this.temp = temp;
-	}
-	public Boolean getLight() {
-		return roomLight;
-	}
-	public void setLight(Boolean light) {
-		this.roomLight = light;
-	}
-	public void checkLight() {
-		System.out.println("Light is On !");
-	}
-	public Boolean getBlind() {
-		return blind;
-	}
-	public void setBlind(Boolean blind) {
-		this.blind = blind;
-	}
+	
+	
 	
 }
