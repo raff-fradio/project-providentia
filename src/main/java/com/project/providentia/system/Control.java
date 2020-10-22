260f2a7043e22a74ce4ac88e0a7e65332a60f973
Raffael Fradio <51884830+raff-fradio@users.noreply.github.com>
2020-10-23 01:12:43 +0700

    Revert "Merge branch 'master' of https://github.com/raff-fradio/project-providentia"
    This reverts commit 02847fbe3168af8b762c358edbbb4191282ecdc2, reversing
    changes made to 66b8c53ed39244448c116de05b50cea287000b0f.

--- src/main/java/com/project/providentia/system/Control.java
+++ src/main/java/com/project/providentia/system/Control.java
@@ -1,39 +1,6 @@
 package com.project.providentia.system;
-
-import java.util.HashMap;
-
 public class Control {
 	
-	private int temperature;
-	private HashMap<String, Boolean> lights;
 	
-	public Control() {
-		this.temperature = 20;
-		this.lights = new HashMap<String, Boolean>();
-		
-		lights.put("Bedroom 1", false);
-		lights.put("Bedroom 2", false);
-		lights.put("Kitchen", false);
-		lights.put("Living Room", false);
-	}
-	
-	public int getTemperature() {
-		return temperature;
-	}
-	public void setTemperature(int temperature) {
-		this.temperature = temperature;
-	}
-	public boolean getLight(String key) {
-		return lights.get(key);
-	}
-	public void setLight(String key, boolean value) {
-		lights.put(key, value);
-	}
-	public HashMap<String, Boolean> getAllLights() {
-		return lights;
-	}
-	public void setAllLights(HashMap<String, Boolean> lights) {
-		this.lights = lights;
-	}
 	
 }
