93378d053d3650732a39b80e4d9dd9e23cfa4f74
raff-fradio <51884830+raff-fradio@users.noreply.github.com>
2020-10-22 16:46:05 +0700

    Added Background & Topbar

--- src/main/java/com/project/providentia/App.java
+++ src/main/java/com/project/providentia/App.java
@@ -1,10 +1,16 @@
 package com.project.providentia;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.stage.WindowEvent;
 
 import com.project.providentia.system.Randomizer;
 
@@ -12,19 +18,28 @@ public class App extends Application {
 
 	@Override
     public void start(Stage primaryStage) throws Exception {
-		Parent root = FXMLLoader.load(getClass().getResource("ui/views/main.fxml"));
+		Parent root = FXMLLoader.load(getClass().getResource("ui/views/Main.fxml"));
 		Scene scene = new Scene(root);
-		scene.getStylesheets().add(getClass().getResource("ui/css/main.css").toExternalForm());
-		
+//		scene.getStylesheets().add(getClass().getResource("ui/css/main.css").toExternalForm());
+                
 		primaryStage.setScene(scene);
+		primaryStage.initStyle(StageStyle.UNDECORATED);
+		primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+		    @Override
+		    public void handle(WindowEvent t) {
+		        Platform.exit();
+		        System.exit(0);
+		    }
+		});
+		
 		primaryStage.show();
     }
 
     public static void main(String[] args) {
-        launch();
     	Randomizer rd = new Randomizer();
     	Thread thread = new Thread(rd);
     	thread.start();
+    	launch();
     }
 
 }
--- src/main/java/com/project/providentia/ui/controllers/MainController.java
+++ src/main/java/com/project/providentia/ui/controllers/MainController.java
@@ -1,16 +1,45 @@
 package com.project.providentia.ui.controllers;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.application.Platform;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
 
 public class MainController implements Initializable {
+	
+	@FXML private ImageView background;
+	@FXML private ImageView closeBtn;
+	@FXML private StackPane closeArea;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
 		
+		initImages();			
+
+	}
+
+	private void initImages() {
+		String bgPath = new File("src/main/resources/com/project/providentia/background.jpg").getAbsolutePath();
+		Image bgImage = new Image(new File(bgPath).toURI().toString());
+		
+		background.setImage(bgImage);
+		
+		String closeBtnPath = new File("src/main/resources/com/project/providentia/close-icon.png").getAbsolutePath();
+		Image closeBtnImage = new Image(new File(closeBtnPath).toURI().toString());
+		
+		closeBtn.setImage(closeBtnImage);		
+	}
+	
+	@FXML
+	private void exit() {
+        Platform.exit();
+		System.exit(0);
 	}
 
 }
